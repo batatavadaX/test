5ea28b7522757a404eaf763614a148d377739d12
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2020-12-18 16:22:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-mkdir nginx
-
-cd nginx
+cd app
 
 wget 'https://openresty.org/download/nginx-1.17.8.tar.gz'
 tar -zxvf nginx-1.8.1.tar.gz
@@ -14,13 +12,6 @@ cd nginx-1.8.1
 
 ./configure --with-http_ssl_module --add-module=../nginx-rtmp-module-master
 
-exit 0
-
-mkdir etc/init.d/nginx
-cd nginx
-wget "https://raw.github.com/JasonGiedymin/nginx-init-ubuntu/master/nginx"
-chmod +x /etc/init.d/nginx
-update-rc.d nginx defaults
 exit 0
 
 chmod 777 wsgi.py
