57dd6a35eacad774a243b1107ebe2d63c978024d
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2020-12-18 16:32:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,10 +8,6 @@ tar -zxvf nginx-1.8.1.tar.gz
 wget 'https://github.com/arut/nginx-rtmp-module/archive/master.zip'
 unzip master.zip 
 
-cd nginx-1.8.1
-
-./configure --with-http_ssl_module --add-module=../nginx-rtmp-module-master
-
 exit 0
 
 chmod 777 wsgi.py
