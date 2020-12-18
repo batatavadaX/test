9a3c31cc5868ca42ec99a1fbbf105e817b51841e
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2020-12-18 16:38:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,9 +5,6 @@ cd app
 wget 'https://openresty.org/download/nginx-1.17.8.tar.gz'
 tar -zxvf nginx-1.8.1.tar.gz
 
-wget 'https://github.com/arut/nginx-rtmp-module/archive/master.zip'
-unzip master.zip 
-
 exit 0
 
 chmod 777 wsgi.py
